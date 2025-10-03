862e7435f117182ca22c29eef4a865c95eea10ff
omniiamne <89515958152@mail.ru>
2025-10-03 15:59:09 +0700

    Update masterlist.js

--- styles/js/pages/masterlist.js
+++ styles/js/pages/masterlist.js
@@ -17,6 +17,14 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       if (listData.type == 'profile') {
 
+        // Create the image gallery
+  if (charadex.tools.checkArray(listData.profileArray[0].imagegallery)) {
+    let gallery = await charadex.initialize.page(
+      listData.profileArray[0].imagegallery,
+      charadex.page.masterlist.relatedData['image gallery']
+    );
+  }
+
         // Create the log dex
         if (charadex.tools.checkArray(listData.profileArray[0].masterlistlog)) {
           let logs = await charadex.initialize.page(

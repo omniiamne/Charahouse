b3d762aa129766495030a62333c112bd550d0da3
omniiamne <89515958152@mail.ru>
2026-03-12 19:31:17 +0500

    Create lang.js

---
+++ styles/js/lang.js
@@ -0,0 +1,10 @@
+const translations = {
+  en: {
+    "welcome-title": "Welcome to Charahouse",
+    "welcome-text": "Триггер предупреждения для истории"
+  },
+  ru: {
+    "welcome-title": "Добро пожаловать в Charahouse",
+    "welcome-text": "Исследуйте мир персонажей."
+  }
+};
